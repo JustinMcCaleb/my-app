09103cdbdd0b6b19c085341094bc9a0aaa7306b2
Justin McCaleb <justinleemccaleb@gmail.com>
2020-09-22 17:25:49 -0500

    added a div and subheading to person

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const person = () => {
-    return <p>I'm a person!</p>
+    return (
+        <div>
+            <p>I'm a person!</p>
+            <h5>This is a subheading</h5>
+        </div>
+    );
 };
 
 export default person;
