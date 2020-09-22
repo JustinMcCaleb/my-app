19791491df7d44dedafe6bbb638adcc39a5b346d
Justin McCaleb <justinleemccaleb@gmail.com>
2020-09-22 16:27:05 -0500

    adding a React.createElement element

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My first REACT app!</h1>
-    </div>
+    // <div className="App">
+    //   <h1>My first REACT app!</h1>
+    // </div>
+      React.createElement('div', null, 'h1', 'I\'m a React.createElement... element')
   );
 }
 
