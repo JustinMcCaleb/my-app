a8c14ea7aa16d4e2c38ae2aaaa99ee5259e5b6d7
Justin McCaleb <justinleemccaleb@gmail.com>
2020-09-22 17:23:17 -0500

    added person component to app

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Person from './Person/Person'
 function App() {
   return (
     <div className="App">
-      <h1>My first REACT app!</h1>
+        <h1>My first REACT app!</h1>
+        <Person />
     </div>
       //the below code is what the above JSX (NOT HTML) code gets COMPILED into. It just serves as an example of what the JSX above code gets turned into with the React import
       // React.createElement('div', {className: 'App'}, React.createElement('h1',null,'I\'m a React.createElement element'))
