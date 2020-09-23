0ee536468ed4c24367b23216768f914c18d9d6e3
Justin McCaleb <justinleemccaleb@gmail.com>
2020-09-23 09:37:21 -0500

    Added content in Person tags in App.js and then added it in Person.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <h1>My first REACT app!</h1>
         <Person name="Justin" age="26"/>
         <Person name="Chey" age="27"/>
-        <Person name="Fluffy" age={234}/>
+        <Person name="Fluffy" age={234}>I am Magic</Person>
     </div>
       //the below code is what the above JSX (NOT HTML) code gets COMPILED into. It just serves as an example of what the JSX above code gets turned into with the React import
       // React.createElement('div', {className: 'App'}, React.createElement('h1',null,'I\'m a React.createElement element'))
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,6 +4,7 @@ const person = (props) => {
     return (
         <div>
             <p>I'm {props.name} and I am {props.age}!</p>
+            <p>{props.children}</p>
         </div>
     );
 };
