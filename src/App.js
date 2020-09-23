29102244f211f320c49f011f8a09c593d686ddd7
Justin McCaleb <justinleemccaleb@gmail.com>
2020-09-23 09:22:24 -0500

    added attributes to Person JSX tags in App

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className="App">
         <h1>My first REACT app!</h1>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Justin" age="26"/>
+        <Person name="Chey" age="27"/>
+        <Person name={"Fluffy"} age={234}/>
     </div>
       //the below code is what the above JSX (NOT HTML) code gets COMPILED into. It just serves as an example of what the JSX above code gets turned into with the React import
       // React.createElement('div', {className: 'App'}, React.createElement('h1',null,'I\'m a React.createElement element'))
