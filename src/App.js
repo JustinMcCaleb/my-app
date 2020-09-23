df2d59cdd002d960cc06acd33b6f67a95310be2e
Justin McCaleb <justinleemccaleb@gmail.com>
2020-09-23 09:53:51 -0500

    added state propety to App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-function App() {
-  return (
+class App extends Component{
+    state = {
+        persons: [
+            { name: 'Justin', age: 26 },
+            { name: 'Chey', age: 27 },
+            { name: 'Fluffy', age: 278}
+        ]
+    }
+
+render() {
+    return (
     <div className="App">
         <h1>My first REACT app!</h1>
-        <Person name="Justin" age="26"/>
-        <Person name="Chey" age="27"/>
-        <Person name="Fluffy" age={234}>I am Magic</Person>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>I am Magic</Person>
     </div>
       //the below code is what the above JSX (NOT HTML) code gets COMPILED into. It just serves as an example of what the JSX above code gets turned into with the React import
       // React.createElement('div', {className: 'App'}, React.createElement('h1',null,'I\'m a React.createElement element'))
   );
+}
 }
 
 export default App;
