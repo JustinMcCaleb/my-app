2d2d69c45045ac79cbb20c1090086c43115f1cb5
Justin McCaleb <justinleemccaleb@gmail.com>
2020-09-23 12:06:19 -0500

    added click property to Person JSX in App

--- src/App.js
+++ src/App.js
@@ -28,9 +28,16 @@ render() {
     <div className="App">
         <h1>My first REACT app!</h1>
         <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>I am Magic</Person>
+        <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}/>
+        <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler}/>
+        <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}>I am Magic</Person>
     </div>
       //the below code is what the above JSX (NOT HTML) code gets COMPILED into. It just serves as an example of what the JSX above code gets turned into with the React import
       // React.createElement('div', {className: 'App'}, React.createElement('h1',null,'I\'m a React.createElement element'))
