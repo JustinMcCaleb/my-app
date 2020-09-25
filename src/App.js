ce48ab4e6720d42cc5ba8a9c9a723069c3565804
Justin McCaleb <justinleemccaleb@gmail.com>
2020-09-25 14:35:36 -0500

    changed person components from beibng hardcoded to being pulled with .map

--- src/App.js
+++ src/App.js
@@ -63,17 +63,23 @@ render() {
     if(this.state.showPersons){
         persons = (
             <div>
-                <Person
-                    name={this.state.persons[0].name}
-                    age={this.state.persons[0].age}/>
-                <Person
-                    name={this.state.persons[1].name}
-                    age={this.state.persons[1].age}
-                    click={this.switchNameHandler.bind(this, 'Justin')}
-                    changed={this.typeNameHandler}/>
-                <Person
-                    name={this.state.persons[2].name}
-                    age={this.state.persons[2].age}>I am Magic</Person>
+                {this.state.persons.map(person => {
+                    return <Person
+                            name={person.name}
+                            age={person.age}
+                            />
+                })}
+                {/*<Person*/}
+                {/*    name={this.state.persons[0].name}*/}
+                {/*    age={this.state.persons[0].age}/>*/}
+                {/*<Person*/}
+                {/*    name={this.state.persons[1].name}*/}
+                {/*    age={this.state.persons[1].age}*/}
+                {/*    click={this.switchNameHandler.bind(this, 'Justin')}*/}
+                {/*    changed={this.typeNameHandler}/>*/}
+                {/*<Person*/}
+                {/*    name={this.state.persons[2].name}*/}
+                {/*    age={this.state.persons[2].age}>I am Magic</Person>*/}
             </div>
         )
     }
